91d3e036b7ce9002a22122346fe12bc4577eeb2e
jackweliss <jackwellis0@gmail.com>
2025-12-02 13:35:17 +0000

    complete update

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,20 @@
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- Customize Treesitter
+
+---@type LazySpec
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      "go",
+      "javascript",
+      "java",
+      "rust",
+      "python"
+      -- add more arguments for adding more treesitter parsers
+    },
+  },
+}
